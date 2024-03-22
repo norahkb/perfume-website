2cc10cf85e9f0dba1767b7649f0c0ebc7810045a
norahkb <163865314+norahkb@users.noreply.github.com>
2024-03-22 04:10:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+(function ($) {
+    ("use strict");
+  
+    // canvas menu activation
+    $(".canvas_open").on("click", function () {
+      $(".offcanvas_menu_wrapper,.off_canvars_overlay").addClass("active");
+    });
+  
+    $(".canvas_close,.off_canvars_overlay").on("click", function () {
+      $(".offcanvas_menu_wrapper,.off_canvars_overlay").removeClass("active");
+    });
+  
+    //   off canvas menu
+    let $offcanvasNav = $(".offcanvas_main_menu"),
+      $offcanvasNavSubMenu = $offcanvasNav.find(".sub-menu");
+    $offcanvasNavSubMenu
+      .parent()
+      .prepend(
+        '<span class="menu-expand"><i class="fa fa-angle-down"></i></span>'
+      );
+  
+    $offcanvasNavSubMenu.slideUp();
+  
+    $offcanvasNav.on("click", "li a, li .menu-expand", function (e) {
+      let $this = $(this);
+      if (
+        $this
+          .parent()
+          .attr("class")
+          .match(/\b(menu-item-has-children| has-children | has-sub-menu)\b/) &&
+        ($this.attr("href") === "#" || $this.hasClass("menu-expand"))
+      ) {
+        e.preventDefault();
+        if ($this.siblings("ul:visible").length) {
+          $this.siblings("ul").slideUp("slow");
+        } else {
+          $this.closest("li").siblings("li").find("ul:visible").slideUp("slow");
+          $this.siblings("ul").slideDown("slow");
+        }
+      }
+  
+      if (
+        $this.is("a") ||
+        $this.is("span") ||
+        $this.attr("class").match(/\b(menu-expand)\b/)
+      ) {
+        $this.parent().toggleClass("menu-open");
+      } else if (
+        $this.is("li") &&
+        $this.attr("class").match(/\b('menu-item-has-children')\b/)
+      ) {
+        $this.toggleClass("menu-open");
+      }
+    });
+  
+    //   search box slidetoggle activation
+    $(".search_box > a").on("click", function () {
+      $(this).toggleClass("active");
+      $(".search_widget").slideToggle("medium");
+    });
+  
+    // slide toggle activation of mini cart
+    $(".mini_cart_wrapper > a").on("click", function () {
+      if ($(window).width() < 991) {
+        $(".mini_cart").slideToggle("medium");
+      }
+    });
+
+})(jQuery);
