f929425e1b6ffa3872b275ec6c9f039294c1290b
Norah binzaid <163865314+norahkb@users.noreply.github.com>
2024-04-03 14:36:40 +0300

    Update main.js

--- main.js
+++ main.js
@@ -105,4 +105,87 @@
       '<i class="fa fa-arrow-right"></i>',
     ],
   }); 
+
+    
+     // product column of 4 activation
+  $(".product_column4")
+  .on("changed.owl.carousel initialized.owl.carousel", function (event) {
+    $(event.target)
+      .find(".owl-item")
+      .removeClass("last")
+      .eq(event.item.index + event.page.size - 1)
+      .addClass("last");
+  })
+  .owlCarousel({
+    autoplay: true,
+    loop: true,
+    nav: true,
+    autoplayTimeout: 5000,
+    items: 4,
+    dots: false,
+    navText: [
+      '<i class="fa fa-arrow-left"></i>',
+      '<i class="fa fa-arrow-right"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+    },
+  });
+  // tooltip activation
+
+  $(".action_links ul li a,.add_to_cart a,.footer_social_link ul li a").tooltip(
+    {
+      animated: "fade",
+      placement: "top",
+      container: "body",
+    }
+  );
+
+  // activation of one column of deal product
+  $(".product_column1")
+    .on("changed.owl.carousel initialized.owl.carousel", function (event) {
+      $(event.target)
+        .find(".owl-item")
+        .removeClass("last")
+        .eq(event.item.index + event.page.size - 1)
+        .addClass("last");
+    })
+    .owlCarousel({
+      autoplay: true,
+      loop: true,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 5000,
+      items: 4,
+      dots: false,
+      navText: [
+        '<i class="fa fa-arrow-left"></i>',
+        '<i class="fa fa-arrow-right"></i>',
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        768: {
+          items: 2,
+        },
+        992: {
+          items: 1,
+        },
+      },
+    });
+
 })(jQuery);
