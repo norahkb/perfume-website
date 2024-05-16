0496a23270b7f9f1be620a2a852a041f3f6a3e11
RAW1010 <157635207+RAW1010@users.noreply.github.com>
2024-05-16 06:16:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -189,3 +189,39 @@
     });
 
 })(jQuery);
+$(".product_column3")
+    .on("changed.owl.carousel initialized.owl.carousel", function (event) {
+      $(event.target)
+        .find(".owl-item")
+        .removeClass("last")
+        .eq(event.item.index + event.page.size - 1)
+        .addClass("last");
+    })
+    .owlCarousel({
+      autoplay: true,
+      loop: true,
+      nav: true,
+      autoplay: false,
+      autoplayTimeout: 5000,
+      items: 4,
+      dots: false,
+      navText: [
+        '<i class="fa fa-arrow-left"></i>',
+        '<i class="fa fa-arrow-right"></i>',
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        567: {
+          items: 2,
+        },
+        768: {
+          items: 3,
+        },
+        992: {
+          items: 3,
+        },
+      },
+    });
